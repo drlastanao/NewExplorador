72abb26ec03c8c6cf833a799706e082c7fa8de47
David Rubio <drlastanao@gmail.com>
2016-08-14 17:52:18 +0200

    v0.1

---
+++ NewExplorador/NewExplorador/Items.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace NewExplorador
+{
+    internal class Items
+    {
+        private FileInfo file;
+        private DirectoryInfo direc;
+
+        private enum Tipo
+        {
+            Archivo,
+            Directorio
+        };
+        private Tipo tipo;
+
+
+        public Items(string nombre,FileInfo i)
+        {
+            file = i;
+            tipo = Tipo.Archivo;
+            
+        }
+
+        public Items(string nombre, DirectoryInfo i)
+        {
+            direc = i;
+            tipo = Tipo.Directorio;
+
+        }
+
+    }
+}
---
+++ NewExplorador/NewExplorador/RecorrerDirectorio.cs
@@ -0,0 +1,62 @@
+﻿
+using System.Collections.Generic;
+using System.IO;
+
+namespace NewExplorador
+{
+    internal class RecorrerDirectorio
+    {
+        private string strDirectorio;
+        private List<Items> itContenido;
+
+
+        public RecorrerDirectorio(string directorio)
+        {
+            strDirectorio = directorio;
+            Recorrer();
+        }
+
+
+        private void Recorrer()
+        {
+            itContenido = new List<Items>();
+                        
+                
+            DirectoryInfo directory = new DirectoryInfo(strDirectorio);
+            FileInfo[] files = directory.GetFiles("*.*");
+            DirectoryInfo[] directories = directory.GetDirectories();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                itContenido.Add(new Items(files[i].FullName, files[i]));
+            }
+
+            for (int i = 0; i < directories.Length; i++)
+            {
+                itContenido.Add(new Items(directories[i].FullName, directories[i]));
+            }
+
+           
+
+        }
+
+
+        public List<Items> filtrar()
+        {
+            List<Items> resultado = new List<Items>();
+            foreach (var item in itContenido)
+            {
+                resultado.Add(item);
+            }
+
+                return resultado;
+
+        }
+
+
+
+
+
+
+    }
+}
